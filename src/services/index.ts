a3ec0cf76495f91f4c5bfe17d718996b67820476
Matthew Warman <warmanmr@gmail.com>
2023-12-17 08:16:17 -0500

    SLSCMN-7 SQS service

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,3 +6,5 @@ export {
 } from './config.service';
 
 export { default as DynamoService } from './dynamo.service';
+
+export { default as SQSService } from './sqs-service';
---
+++ src/services/sqs-service.ts
@@ -0,0 +1,32 @@
+import {
+  SQSClient,
+  SQSClientConfig,
+  SendMessageCommand,
+  SendMessageCommandInput,
+  SendMessageCommandOutput,
+} from '@aws-sdk/client-sqs';
+
+import { lambdaConfigValues as config } from './config.service';
+
+const clientConfig: SQSClientConfig = {
+  region: config.AWS_LAMBDA_FUNCTION_VERSION,
+};
+
+const sqsClient = new SQSClient(clientConfig);
+
+/**
+ * Send a message to a queue.
+ * @param input - A `SendMessageCommandInput` object.
+ * @returns A Promise which resolves to a `SendMessageCommandOutput` object.
+ */
+const sendMessage = (input: SendMessageCommandInput): Promise<SendMessageCommandOutput> => {
+  return sqsClient.send(new SendMessageCommand(input));
+};
+
+/**
+ * Use the `SQSService` to interact with AWS Simple Queue Service.
+ * @see {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/sqs/ | AWS SDK SQSClient}
+ */
+const SQSService = { sendMessage };
+
+export default SQSService;
