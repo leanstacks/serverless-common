3d6ff71bc6e891b60d0bd1b2896b53c076b84530
Matthew Warman <warmanmr@gmail.com>
2023-12-19 19:31:41 -0500

    SLSCMN-13 logging

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,5 +4,5 @@ process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE = '128';
 process.env.AWS_LAMBDA_FUNCTION_NAME = 'aws-lambda-function-name';
 process.env.AWS_LAMBDA_FUNCTION_VERSION = 'aws-lambda-function-version';
 process.env.AWS_REGION = 'aws-region';
-process.env.LOGGING_ENABLED = 'true';
-process.env.LOGGING_LEVEL = 'debug';
+process.env.LOGGING_ENABLED = 'false';
+process.env.LOGGING_LEVEL = 'error';
